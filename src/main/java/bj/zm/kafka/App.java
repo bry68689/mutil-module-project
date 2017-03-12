17a0ec3b2c6bd4927fcc34ef157a79408d3c6a43
junqiang.shi <junqiang.shi@timanetworks.com>
2017-03-12 06:15:23 -0700

    test git flow

--- src/main/java/bj/zm/kafka/App.java
+++ src/main/java/bj/zm/kafka/App.java
@@ -4,10 +4,12 @@ package bj.zm.kafka;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Test Git FLow!" );
+
     }
 }
